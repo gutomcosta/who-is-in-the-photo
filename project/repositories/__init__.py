12864e400c4bc70792e8a00908b9bb52f63b2dd2
Luiz Costa <gutomcosta@gmail.com>
2017-12-07 01:10:43 -0200

    tests the photo repository

---
+++ project/repositories/__init__.py
@@ -0,0 +1,1 @@
+from .photo_repository import *
--- project/repositories/photo_repository.py
+++ project/repositories/photo_repository.py
@@ -1,11 +1,12 @@
 import face_recognition
+from domain import Photo
+
 
 class PhotoRepository(object):
 
-    def get_photo_from(path):
-        image = face_recognition.load_image_file(path)
-        # locations =  face_recognition.face_locations(image)
-        # for (top, right, bottom, left)  in locations:
-        #     frame = cv2.imread('../obama.jpeg')
-        #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        #     cv2.imwrite("ah.png",frame)
+    def get_photo_from(self,path):
+        try:
+            image = face_recognition.load_image_file(path)
+            return Photo(image, path)
+        except IOError:
+            raise ValueError("Invalid path to image {}".format(path))
