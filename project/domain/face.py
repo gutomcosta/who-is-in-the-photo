73fadfb863d3c2a3ed6f5d790be6c2797b6b4f57
Luiz Costa <gutomcosta@gmail.com>
2017-12-09 03:36:08 -0200

    experiment expand face boxes

--- project/domain/face.py
+++ project/domain/face.py
@@ -1,6 +1,16 @@
 class Face(object):
-    def __init__(self, top, right, bottom, left):
+    def __init__(self, image, top, right, bottom, left):
         self.top = top
         self.right = right
         self.bottom = bottom
         self.left = left
+        self.original_image = image
+
+    
+    def image(self):
+        # top = self.top - 30 > self.ori
+        
+        return self.original_image[self.top-30:self.bottom+30, self.left-30:self.right+30]
+        # return self.original_image[self.top:self.bottom, self.left:self.right]
+        
+
