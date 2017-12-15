b95ef01f981dcb83b55e452f78ee4c859ae7d855
Luiz Costa <gutomcosta@gmail.com>
2017-12-15 01:14:54 -0200

    pass an image instead a face to neural network

--- project/tests/services/test_person_identifier.py
+++ project/tests/services/test_person_identifier.py
@@ -14,11 +14,13 @@ class PersonIdentifierTest(unittest.TestCase):
         self.neural_network = Mock()
         self.person_identifier = PersonIdentifier(self.neural_network)
         self.neural_network.predict.return_value = {'predicted_class': 'Barack Obama', 'confidence': 0.9}
+        self.image = Mock()
         self.face = Mock()
+        self.face.image.return_value = self.image
     
     def test_it_asks_to_neural_net_for_identify_a_person(self):
         self.person_identifier.identify([self.face])
-        self.neural_network.predict.assert_called_with(self.face)
+        self.neural_network.predict.assert_called_with(self.image)
     
     def test_it_creates_a_person_object_for_each_face(self):
         persons = self.person_identifier.identify([self.face])
